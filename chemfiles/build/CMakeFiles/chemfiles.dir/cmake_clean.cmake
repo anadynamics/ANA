196b77b289ab62136e875a250440f2484838de91
pbarletta <pbarletta@gmail.com>
2018-05-29 19:33:01 -0300

    Not tracking chemfiles/build and chemfiles/install anymore

--- chemfiles/build/CMakeFiles/chemfiles.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libchemfiles.pdb"
-  "libchemfiles.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C CXX)
-  include(CMakeFiles/chemfiles.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- chemfiles/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/german/labo/16/ANA/chemfiles
-# Build directory: /home/german/labo/16/ANA/chemfiles/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(external)
