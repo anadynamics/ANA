db5bc8f01561830cf20fea9c0874a83853b68ec4
German <pbarletta@gmail.com>
2018-10-25 20:51:00 -0300

    Minor fixes ond ndd.jl although it's still not working. Something to do with StaticArrays and converting Julia Arrays into MVector/MMatrix

--- scripts/ndd.jl
+++ scripts/ndd.jl
@@ -1,4 +1,4 @@
-#!/usr/bin/env julia
+#! /usr/local/env julia
 ###############################################################################
 # Utility to displace a PDB along many vectors, generating 1 PDB for each
 # vector, useful for NDD analysis.
@@ -32,12 +32,12 @@ function displaceAA(in_frm, aa, aa_3, in_vec)
     cursor = 0
     for i = 1:aa
         if i == 1
-            sum_mat[:, 1:natom_aa[i]] = repmat(vector[:, 1], 1, natom_aa[i])
+            sum_mat[:, 1:natom_aa[i]] = repeat(vector[:, 1], 1, natom_aa[i])
             cursor = natom_aa[i]
             continue
         end
         rango = collect(cursor+1:cursor + natom_aa[i])
-        sum_mat[:, rango] = repmat(vector[:, i], 1, natom_aa[i])
+        sum_mat[:, rango] = repeat(vector[:, i], 1, natom_aa[i])
         cursor += natom_aa[i]
     end
 
@@ -48,7 +48,7 @@ function displaceAA(in_frm, aa, aa_3, in_vec)
     # Tengo q hacer esto por ahora. Hasta q arreglemos Chemfiles.
     for i = 1:size(in_xyz)[1]
         for j = 1:size(in_xyz)[2]
-            out_xyz[i, j]  = round(in_xyz[i, j] + sum_mat[i, j], 3)
+            out_xyz[i, j]  = round(in_xyz[i, j] + sum_mat[i, j], digits = 3)
         end
     end
     return out_frm
@@ -74,20 +74,16 @@ s = ArgParseSettings()
         arg_type = String
         required = true
     "--amber_modes", "-a"
-        help = "Mark true when reading from Amber PCA modes kind of file. Default: false."
+        help = "Mark true when reading Amber(format) modes. Default: false."
         arg_type = Bool
         required = false
         default = false
     "--weights_filename", "-w"
-        help = "Input 1/weights, if desired. Default: none"
+        help = "Input 1/weights, if desired. If Amber modes are read, eigenvalues will be used. Default: none"
         arg_type = String
         required = false
         default = "none"
 end
-
-##########
-# main program
-##########
 
 # Read arguments from console
 parsed_args = parse_args(ARGS, s)
@@ -110,67 +106,39 @@ const in_frm = read(in_trj)
 const in_top = Topology(in_frm)
 const aa = convert(Int64, count_residues(in_top))
 const aa_3 = aa * 3
-# Initialize modes vectors
-in_modes = Array{Float64, 2}(undef, aa_3, aa)
-in_evals = Array{Float64, 1}(undef, aa_3)
+nmodes = aa_3 - 6
+
+# Read input vectors and weights
+in_modes = MMatrix{aa_3, nmodes, Float64}(undef)
+weights = MVector{nmodes, Float64}(undef)
 if (amber_modes)
     try
-        in_modes, in_evals = JUMD.readPtrajModes(modes_filename)
+        tmp_modes, tmp_weights = JUMD.readPtrajModes(modes_filename)
+        in_modes = convert(MMatrix{aa_3, nmodes, Float64}, tmp_modes)
+        weights = convert(MVector{nmodes, Float64}, tmp_weights)
     catch e
-        println("")
         println("Error when reading Amber modes: ", modes_filename)
         error(e)
     end
 else
     try
-        in_modes = convert(Array{Float64, 2}, readdlm(modes_filename))
+        in_modes = convert(MMatrix{aa_3, nmodes, Float64}, readdlm(modes_filename))
     catch e
-        println(modes_filename, " error:")
+        println("Error when reading modes: ", modes_filename)
         error(e)
     end
-end
-const l_modes = size(in_modes)[1]
-const n_modes = size(in_modes)[2]
-# Check input modes
-if l_modes != aa_3
-# Asumo q el modo es de Calpha y está ordenado en una columna
-    error(string("\n\n", " Input PDB has ", aa, " residues, so input ",
-        "modes should be ", aa_3, " long."))
+    if weights_filename != "none"
+        try
+            weights = convert(MVector{nmodes, Float64}, readdlm(weights_filename))
+        catch e
+            println("Error when reading weights: ", weights_filename)
+            error(e)
+        end
+    else
+        weights = convert(MVector{nmodes, Float64}, fill(1., nmodes))
+    end
 end
 
-# Initialize weights
-weights = MVector{n_modes, Float64}()
-if (amber_modes)
-    if weights_filename != "none"
-        try
-            weights = convert(MVector{n_modes, Float64},
-                readdlm(weights_filename)[:, 1])
-        catch e
-            println(weights_filename, " error:")
-            error(e)
-        end
-    else
-        try
-            weights = convert(MVector{n_modes, Float64}, in_evals)
-        catch e
-            println("Number of amber PCA eigenvalues does not match number of ",
-                "PCA modes")
-            error(e)
-        end
-    end
-else
-    if weights_filename != "none"
-        try
-            weights = convert(MVector{n_modes, Float64},
-                readdlm(weights_filename)[:, 1])
-        catch e
-            println(weights_filename, " error:")
-            error(e)
-        end
-    else
-        weights = convert(MVector{n_modes, Float64}, fill(1.0, n_modes))
-    end
-end
 
 # Ahora desplazo
 pdb_names = Array{String}(undef, n_modes)
@@ -184,6 +152,7 @@ for i = 1:n_modes
     out_trj = Trajectory(pdb_names[i], 'w')
     write(out_trj, out_frm)
 end
+
 # Write in_ndd file
 const in_ndd_filename = string("in_ndd_",
     splitext(basename(in_pdb_filename))[1])
